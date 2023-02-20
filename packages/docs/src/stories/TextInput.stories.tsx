5c73d93430e9f9465f15d5ed139826f7d817b784
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-02-20 12:23:26 -0300

    fix: Add ref to TextInput and adjust styles

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react'
-import { Box, Text, TextInput, TextInputProps } from '@amos-ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react';
+import { Box, Text, TextInput, TextInputProps } from '@amos-ignite-ui/react';
 
 export default {
   title: 'Form/Text Input',
@@ -10,30 +10,30 @@ export default {
       return (
         <Box
           as="label"
-          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
-        >
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}>
           <Text size="sm">Email address</Text>
           {Story()}
         </Box>
-      )
+      );
     },
   ],
-} as Meta<TextInputProps>
+} as Meta<TextInputProps>;
 
 export const Primary: StoryObj<TextInputProps> = {
   args: {
     placeholder: 'Type your name',
   },
-}
+};
 
 export const Disabled: StoryObj<TextInputProps> = {
   args: {
     disabled: true,
   },
-}
+};
 
 export const WithPrefix: StoryObj<TextInputProps> = {
   args: {
     prefix: 'cal.com/',
+    placeholder: 'your-username',
   },
-}
+};
